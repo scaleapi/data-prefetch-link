79995bfed22dd237bdc9b0fc6a3f51cd7f7d8a12
Kennan Davison <12569707+kennandavison@users.noreply.github.com>
2018-03-12 02:04:43 -0700

    Fix withData propTypes warning

--- src/link.js
+++ src/link.js
@@ -1,15 +1,33 @@
+import exact from 'prop-types-exact';
 import Link from 'next/link';
-import { resolve, parse, format } from 'url';
+import PropTypes from 'prop-types';
 import Router from 'next/router';
+import { resolve, parse, format } from 'url';
+
+// Copied from 'prop-types-exact'
+const zeroWidthSpace = '\u200b';
+const specialProperty = `prop-types-exact: ${zeroWidthSpace}`;
 
 export default class DataPrefetchLink extends Link {
+  static getLinkPropTypesNoExact() {
+    const linkPropTypes = { ...Link.propTypes };
+    delete linkPropTypes[specialProperty];
+    return linkPropTypes;
+  }
+
+  static propTypes = exact({
+    ...DataPrefetchLink.getLinkPropTypesNoExact(),
+    withData: PropTypes.bool,
+  });
+
   prefetch() {
     if (typeof window === 'undefined' || !this.props.prefetch) {
       return;
     }
 
     const { pathname } = window.location;
-    const hrefString = this.props.href !== 'string' ? format(this.props.href) : this.props.href;
+    const hrefString =
+      this.props.href !== 'string' ? format(this.props.href) : this.props.href;
     const href = resolve(pathname, hrefString);
     const { query } = parse(href, true);
 
